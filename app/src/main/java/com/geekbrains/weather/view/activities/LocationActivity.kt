f3a16518f97c2b082751f4e2bd20fe71e86aab93
Chester095 <Chester095@users.noreply.github.com>
2022-02-10 20:17:00 +0300

    Lesson 12 спрятал API карт

--- app/src/main/java/com/geekbrains/weather/view/activities/LocationActivity.kt
+++ app/src/main/java/com/geekbrains/weather/view/activities/LocationActivity.kt
@@ -144,7 +144,7 @@ class LocationActivity : AppCompatActivity(), OnMapReadyCallback {
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
-        Log.d("!!! onMapReady","mMap"+mMap)
+        Log.d("!!! onMapReady", "mMap$mMap")
         // Add a marker in Sydney and move the camera
         val sydney = LatLng(-34.0, 151.0)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
